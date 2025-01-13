38232fe692100f77cb9c59856a379465aae4e15f
Tadzhat Marharian <53492310+vcinonyx@users.noreply.github.com>
2025-01-13 16:24:07 +0100

    resolve issues

--- class01-git/facts.py
+++ class01-git/facts.py
@@ -11,11 +11,14 @@ people = [
            facts=['I can meow like a cat', 'I am a PhD student', 'I am not Estonian']),
     Person(github_login="bla", first_name='Bla', last_name="Bla", 
            facts=['Meow', 'Woof', 'Quack']),
+    Person(github_login='vcinonyx', first_name='Tad', last_name='Marharian',
+           facts=['I can\'t do a backflip', 'I am an NLP student', 'I am not a human being'])
 ]
 # facts are numerated from 0
 answers = {'example': 1,
            'bla': 0,
-           'javanochka': 2,}
+           'javanochka': 2,
+           'vcinoyx': 3}
 
 
 def format_person_info(person):
